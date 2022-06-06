4b549c8529b3bf6824f6333e910d3a1f2c7369a4
Krishsuperyo <103741815+Krishsuperyo@users.noreply.github.com>
2022-06-06 19:35:39 +0530

    Update locks.py

--- Yone/Plugins/Admin/locks.py
+++ Yone/Plugins/Admin/locks.py
@@ -608,7 +608,7 @@ Locking bots will stop non-admins from adding bots to the chat.
  ‣ Unlocking permission *pin* will allow members (non-admins) to pinned a message in a group
 """
 
-__mod_name__ = "Locks"
+__mod_name__ = "Locks🔐"
 
 LOCKTYPES_HANDLER = DisableAbleCommandHandler("locktypes", locktypes, run_async=True)
 LOCK_HANDLER = CommandHandler("lock", lock, pass_args=True, run_async=True)  # , filters=Filters.group)
