abe3d99c041075175f59720764cc78701db97273
Krishsuperyo <103741815+Krishsuperyo@users.noreply.github.com>
2022-06-06 19:42:52 +0530

    Update antiflood.py

--- Yone/Plugins/Admin/antiflood.py
+++ Yone/Plugins/Admin/antiflood.py
@@ -407,7 +407,7 @@ will result in restricting that user.
  `1w` = 1 week
  """
 
-__mod_name__ = "Anti-Flood"
+__mod_name__ = "Anti-Flood🛡"
 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update, check_flood, run_async=True
